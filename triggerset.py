ccc51e0bb3435c38d4c17745c6f882e669dd120f
Veron Clément <clement.veron@student-cs.fr>
2022-10-20 11:38:20 +0200

    trigger1

--- triggerset.py
+++ triggerset.py
@@ -1,13 +1,18 @@
-def get_triggerset(trigger_params:dict) -> tbd:
+from dataset import get_dataset
+
+
+def get_triggerset(trigger_params: dict) -> tbd:
     '''
     to be called by main.py or else to get the correct triggerset
     different generation methods may be used
 
     trigger_params:dict please refer to nomenclature.txt 
     '''
-    #todo create all three methods of triggerset gen
+    # todo create all three methods of triggerset gen
     # you can use folder ./triggersets to store images
+    dataset = get_dataset(data_params)
     raise NotImplementedError()
+
 
 if __name__ == " __main___":
     # test when coding this module
