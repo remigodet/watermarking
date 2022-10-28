78f8741459cc92f7c4d4040588d0f22bfcc307c5
thomasBouiniere <thomas.bouiniere@gmail.com>
2022-10-28 11:55:06 +0200

    dataset <- get_dataset

--- dataset.py
+++ dataset.py
@@ -1,10 +1,40 @@
-def get_dataset(data_params:dict) -> tbd:
-    '''
-    to be called by main.py to get the correct dataset 
-    data_params:dict please refer to nomenclature.txt 
-    '''
-    raise NotImplementedError()
+import tensorflow as tf
+from keras import layers, datasets, models
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+
+
+def get_dataset(data_params: dict):
+    set = data_params["set"]
+    n = data_params["n"]
+    lenght = 10000
+    if data_params["dataset"] == "cifar-10":
+        (X_pretest, y_pretest), (X_test, y_test) = datasets.cifar10.load_data()
+    X_trigger, y_trigger = X_pretest[0:lenght], y_pretest[0:lenght]
+    X_train, y_train = X_pretest[lenght:], y_pretest[lenght:]
+    # renvoyer n images aléatoire parmi le "set" entré en argument
+    if set == "train":
+        X = X_train
+        y = y_train
+    elif set == "test":
+        X = X_test
+        y = y_test
+    else:
+        X = X_trigger
+        y = y_trigger
+    X = list(X)
+    index = np.random.randint(len(X), size=n)
+    X_a = []
+    y_a = []
+    for i in index:
+        X_a.append(X[i])
+        y_a.append(y[i])
+    return(X_a, y_a)  # X_set_sample.shape =( n, 32, 32, 3) , y_set_sample = (n,1)
+
 
 if __name__ == " __main___":
     # test when coding this module
-    pass
+    X, y = get_dataset({"set": "train", "n": 10, "dataset": "cifar-10"})
+    plt.imshow(X[0])
+    print(y[0])
