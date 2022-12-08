4941928353aa0a0e9e4744e51ddd691b9b963e67
thomasBouiniere <thomas.bouiniere@gmail.com>
2022-12-08 09:20:58 +0100

    ok

---
+++ analysis/metrics 2.py
@@ -0,0 +1,19 @@
+#import
+
+
+# public func
+def metric(model,analysis_params) -> str:
+    '''
+    function to analyse model based on the analysis_params["analysis"] list
+    Please refer to nomenclature.md for syntax
+    '''
+
+    # construct the nomenclature as you want
+    # list mean several analysis will be conducted, you need to find the one concerning this module
+    # you can modify the list  analysis_params["analysis"] items to tuple ("res", your_res:str)
+    print("metric function called")
+    pass
+#testing
+if __name__ == "__main__":
+    #/!> don't forget to import you rmodule in main.py and place it in the dict just below
+    pass
